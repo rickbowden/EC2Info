aa4f5a5d8a0dc1863373ccc986b6187157e82343
rick-bowden <rick.bowden@kcom.com>
2017-02-08 12:06:18 +0000

    r

--- EC2Info/App.cs
+++ EC2Info/App.cs
@@ -26,8 +26,7 @@ namespace EC2Info
         public App()
         {
             InitializeComponent();
-
-                      
+                                  
 
             PopulateRegionDropDown();
             PopulateProfileDropDown();

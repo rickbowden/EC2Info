e032786477dd350e59822f9fecf7d60b94add9cb
rickbowden <rick@areyoulocal.me.uk>
2017-03-14 15:13:19 +0000

    updated version

--- EC2Info/Properties/Settings.Designer.cs
+++ EC2Info/Properties/Settings.Designer.cs
@@ -105,7 +105,7 @@ namespace EC2Info.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0.5")]
+        [global::System.Configuration.DefaultSettingValueAttribute("0.6")]
         public string Version {
             get {
                 return ((string)(this["Version"]));
